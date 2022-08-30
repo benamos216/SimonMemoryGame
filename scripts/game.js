adc11511af42bec364928ba86f1cbd86aecbc5bc
benamos216 <benamos216@gmail.com>
2022-08-30 20:30:19 +0000

    More Advanced Tests - Part 2

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,35 @@
+const { set } = require("lodash");
+
 let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
+    choices: ["button1", "button2", "button3", "button4"],
 };
 
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
 
-module.exports = { game };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
